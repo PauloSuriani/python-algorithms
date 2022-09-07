41760925a797aa1d7ee6ae34a7a673eef182cb4a
PauloSuriani <paulosuriani@gmail.com>
2022-09-07 09:01:03 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    for student in permanence_period:
+        if not (isinstance(student[0], int) and isinstance(student[1], int)):
+            return None
+
+        if target_time >= student[0] and target_time <= student[1]:
+            counter += 1
+
+    return counter
